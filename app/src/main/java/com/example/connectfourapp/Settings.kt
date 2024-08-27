7812737ef611cfb22f9af749aec665591e49e927
Jet Le <jet05le@gmail.com>
2024-08-27 20:33:27 +0800

    FEAT: Name Text View

--- app/src/main/java/com/example/connectfourapp/Settings.kt
+++ app/src/main/java/com/example/connectfourapp/Settings.kt
@@ -25,8 +25,14 @@ import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.Button
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Icon
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -42,6 +48,9 @@ import com.example.connectfourapp.ui.theme.Righteous
 //---------- Settings Screen Layout
 @Composable
 fun SettingsScreen(){
+    // Player 1 and 2 Names
+    var playerOneName by remember { mutableStateOf("Player 1") }
+    var playerTwoName by remember { mutableStateOf("Player 2") }
 
     Column(
         modifier = Modifier
@@ -58,7 +67,7 @@ fun SettingsScreen(){
             modifier = Modifier
                 .fillMaxSize()
                 .background(GreyBG)
-                .padding(horizontal = 30.dp),
+                .padding(horizontal = 5.dp),
             horizontalAlignment = Alignment.CenterHorizontally,
             verticalArrangement = Arrangement.SpaceEvenly
         ) {
@@ -69,12 +78,54 @@ fun SettingsScreen(){
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 //---------- Player 1 Customisation
-                Column() {
-                    Text(text = "sd")
+                Box(
+                    modifier = Modifier.weight(1f)
+                ) {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally,
+                        modifier = Modifier.fillMaxWidth()
+                    ) {
+                        Text(text = "Player 1", fontSize = 20.sp)
+                        Image(
+                            modifier = Modifier
+                                .size(64.dp),
+                            imageVector = Icons.Default.AccountCircle,
+                            contentDescription = "Player 1 Profile Pic"
+                        )
+                        OutlinedTextField(
+                            value = playerOneName,
+                            onValueChange = { newPlayerOneName ->
+                                playerOneName = newPlayerOneName
+                            },
+                            label = { Text(text = "Player 1 name...") },
+                        )
+                    }
                 }
+
+                Spacer(modifier = Modifier.width(16.dp)) // Spacer
+
                 //---------- PLayer 2 Customisation
-                Column {
-                    Text(text = "sd")
+                Box(modifier = Modifier.weight(1f))
+                {
+                    Column(
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        Text(text = "Player 2", fontSize = 20.sp)
+                        Image(
+                            modifier = Modifier
+                                .size(64.dp),
+                            imageVector = Icons.Default.AccountCircle,
+                            contentDescription = "Player 2 Profile Pic"
+                        )
+                        // Player 2 Name
+                        OutlinedTextField(
+                            value = playerTwoName,
+                            onValueChange = { newPlayerTwoName ->
+                                playerTwoName = newPlayerTwoName
+                            },
+                            label = { Text(text = "Player 2 name...") },
+                        )
+                    }
                 }
             }
 
