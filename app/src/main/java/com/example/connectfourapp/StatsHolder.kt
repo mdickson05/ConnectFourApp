527aad159fd183c91117a0d10d303ed374c45bf7
mdickson05 <124563538+mdickson05@users.noreply.github.com>
2024-09-17 14:08:10 +0800

    FEAT: Store PlayerStats on database

---
+++ app/src/main/java/com/example/connectfourapp/StatsHolder.kt
@@ -0,0 +1,9 @@
+package com.example.connectfourapp
+
+data class StatsHolder(
+    val id : String,
+    val PlayerOneSP : PlayerStats,
+    val PlayerOneMP : PlayerStats,
+    val PlayerTwo : PlayerStats,
+    val AI : PlayerStats
+)
